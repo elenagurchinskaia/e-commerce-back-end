e646ac2c3e699736b1679cd06d53eb81efaf03ae
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-08-07 21:03:41 -0500

    get all products router

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,50 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+const Product = require("./Product");
+const Tag = require("./Tag");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // * `id`
+    // * Integer.
+    // * Doesn't allow null values.
+    // * Set as primary key.
+    // * Uses auto increment.
+    //====================//
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: Product, key: "id" },
+    },
+    //   * `product_id`
+    //   * Integer.
+    //   * References the `Product` model's `id`.
+    //====================//
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: Tag, key: "id" },
+    },
+
+    // * `tag_id`
+    //   * Integer.
+    //   * References the `Tag` model's `id`.
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,39 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
+
+  //* `id`
+  // * Integer.
+  // * Doesn't allow null values.
+  // * Set as primary key.
+  // * Uses auto increment.
+  //====================//
+  // * `tag_name`
+  // * String.
+
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
