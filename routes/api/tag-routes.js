2cfe95b4dac60a405831ca84fabacfdce0bc6218
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-08-07 23:26:00 -0500

    delete router for tag-router.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,7 +24,7 @@ router.get("/:id", async (req, res) => {
       include: [{ model: Product }],
     });
     if (!tagData) {
-      res.status(404).json({ message: 'No tag found with this id!'});
+      res.status(404).json({ message: "No tag found with this id!" });
       return;
     }
     res.status(200).json(tagData);
@@ -38,9 +38,8 @@ router.post("/", async (req, res) => {
     // create a new tag
     const newTag = await Tag.create(req.body);
     res.status(201).json(newTag);
-    } catch (err) {
+  } catch (err) {
     res.status(400).json(err);
-
   }
 });
 
@@ -61,7 +60,18 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   try {
     // delete on tag by its `id` value
-    
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deletedTag) {
+      res.status(404).json({ message: "No tag found with this id!" });
+      return;
+    }
+    res.status(200).json(deletedTag);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
